89150a9fe05a046098333b2c57bacb0a0cd95334
Aryormdhnn <67365918+aryormdhnn@users.noreply.github.com>
2023-03-31 13:08:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// Soal Nomor 1 : 
+for (var i = 1; i <= 100; i++) {
+    console.log("User ke - " + i);
+}
+// Soal Nomor 2: 
+var nilaiAwal = 0;
+var jumlahPengulangan = 10;
+
+for (var i = 0; i < jumlahPengulangan; i++) {
+  nilaiAwal += 2;
+  console.log("Nilai saat ini: " + nilaiAwal);
+}
+
+// Soal Nomor 3:
+for (var i = 0; i <= 20; i++) {
+    if (i % 2 === 0) {
+      console.log(i + " adalah bilangan genap");
+    } else {
+      console.log(i + " adalah bilangan ganjil");
+    }
+  }
+
+// Soal Nomor 4:
+  var jumlahKlikOK = 0;
+
+  while (true) {
+    var konfirmasi = confirm("Apakah anda mau mengulang?");
+    if (konfirmasi === true) {
+      jumlahKlikOK++;
+    } else {
+      console.log("Perulangan sudah dilakukan sebanyak " + jumlahKlikOK + " kali.");
+      break;
+    }
+  }
+
+  // Soal Nomor 4:
+  var jawaban = "Impact Byte";
+
+while (true) {
+  var input = prompt("Sebutkan kepanjangan dari nama IB ?");
+  if (input === jawaban) {
+    alert("Selamat, jawaban kamu benar!");
+    break;
+  } else {
+    alert("Maaf, jawaban kamu salah. Silakan coba lagi.");
+  }
+}
